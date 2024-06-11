eeeea09e66fb2d464ebb7192edfa4d2c2200bdc0
Antonio Sarmiento <zarmiento227@gmail.com>
2024-06-11 16:56:34 -0500

    adicional antes de irme

--- typscript-training/01-helloword/myhelloword.js
+++ typscript-training/01-helloword/myhelloword.js
@@ -3,3 +3,6 @@ console.log("coding gurus" + "time for fun");
 var found = true;
 var firdt = 0.265;
 var whatever = [{ objeto: "valor" }];
+var firstname = "Antonio";
+var lastname = "Sarmiento";
+console.log("Hola ".concat(firstname, " ").concat(lastname));
--- typscript-training/01-helloword/myhelloword.ts
+++ typscript-training/01-helloword/myhelloword.ts
@@ -4,3 +4,8 @@ console.log("coding gurus"+"time for fun")
 let found: boolean = true;
 let firdt: number = 0.265;
 let whatever: any = [{objeto:"valor"}];
+
+let firstname: string = "Antonio";
+let lastname: string = "Sarmiento";
+
+console.log(`Hola ${firstname} ${lastname}`);
