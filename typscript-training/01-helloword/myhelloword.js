69154aaccac80634cef6e87cf4e5379cb8f0ffe1
Antonio Sarmiento <zarmiento227@gmail.com>
2024-06-11 16:51:30 -0500

    initial commit

---
+++ typscript-training/01-helloword/myhelloword.js
@@ -0,0 +1,5 @@
+console.log("hello world");
+console.log("coding gurus" + "time for fun");
+var found = true;
+var firdt = 0.265;
+var whatever = [{ objeto: "valor" }];
---
+++ typscript-training/01-helloword/myhelloword.ts
@@ -0,0 +1,6 @@
+console.log("hello world")
+console.log("coding gurus"+"time for fun")
+
+let found: boolean = true;
+let firdt: number = 0.265;
+let whatever: any = [{objeto:"valor"}];
